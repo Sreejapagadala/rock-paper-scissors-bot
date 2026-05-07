e70480243ad5c459c693c6fa1cbd69095b7710f8
Sreeja Pagadala <122866553+Sreejapagadala@users.noreply.github.com>
2026-05-08 03:08:45 +0530

    Create main.py to play RPS against various players
    Added main.py to run RPS game against multiple players.

---
+++ main.py
@@ -0,0 +1,14 @@
+from RPS_game import play, quincy, abbey, kris, mrugesh
+from RPS import player
+
+print("Playing against Quincy...")
+play(player, quincy, 1000, verbose=False)
+
+print("Playing against Abbey...")
+play(player, abbey, 1000, verbose=False)
+
+print("Playing against Kris...")
+play(player, kris, 1000, verbose=False)
+
+print("Playing against Mrugesh...")
+play(player, mrugesh, 1000, verbose=False)
